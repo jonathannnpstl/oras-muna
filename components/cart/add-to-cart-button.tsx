aecd80370239b7b54cb2730ca7092902692f091a
jonathannnpstl <jonathannnpstl@gmail.com>
2023-12-31 23:36:44 +0800

    commit before NEW YEAR!!!

--- components/cart/add-to-cart-button.tsx
+++ components/cart/add-to-cart-button.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { useFormState, useFormStatus } from "react-dom";
 import { addItem } from "./actions";
 
-export default function AddToCartButton({ id }: { id: string }) {
+export default function AddToCartButton({
+  id,
+  quantity,
+}: {
+  id: string;
+  quantity: number;
+}) {
   //want to make the cart modal to open when add to cart button is clicked
   //rn, i dont know how to do that
 
@@ -11,16 +17,16 @@ export default function AddToCartButton({ id }: { id: string }) {
   //Store it in the local storage, the 'user id' and if unknown user
   //signed up, the user id will be replaced by the email of the user
 
-  const handleSubmit = () => {
-    addItem(id, 1);
-  };
-
+  const [message, formAction] = useFormState(addItem, null);
+  const actionWithId = formAction.bind(null, { id: id, quantity });
+  /**
+   * include date when the adding item happened
+   */
   return (
-    <button
-      onClick={handleSubmit}
-      className="showcase-btn bg-green-500 text-white w-full"
-    >
-      ADD TO CART
-    </button>
+    <div className="showcase-btn bg-green-500 text-white w-full flex items-center justify-center">
+      <form className="text-center" action={actionWithId}>
+        <button type="submit">ADD TO CART</button>
+      </form>
+    </div>
   );
 }
--- components/cart/edit-qty-button.tsx
+++ components/cart/edit-qty-button.tsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { useFormState, useFormStatus } from "react-dom";
+import { updateItemQuantity } from "./actions";
+import { Product } from "@/lib/definition";
 
 export default function EditQtyButton({
   operation,
-  handleClick,
+  item,
 }: {
   operation: "plus" | "minus";
-  handleClick: (operation: "-" | "+") => void;
+  item: { productId: string; quantity: number };
 }) {
+  const [message, formAction] = useFormState(updateItemQuantity, null);
+  const actionWithId = formAction.bind(null, {
+    id: item?.productId,
+    quantity: operation === "plus" ? item?.quantity + 1 : item?.quantity - 1,
+  });
+
   /**
    * Evry click should update the database
    */
   return (
-    <button
-      onClick={() => handleClick(operation === "plus" ? "+" : "-")}
-      className="ease flex h-full min-w-[36px] max-w-[36px]  flex-none items-center justify-center px-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-80"
-    >
-      {operation === "plus" ? <span>+</span> : <span>-</span>}
-    </button>
+    <form action={actionWithId}>
+      <button
+        type="submit"
+        className="ease flex h-full min-w-[36px] max-w-[36px]  flex-none items-center justify-center px-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-80"
+      >
+        {operation === "plus" ? <span>+</span> : <span>-</span>}
+      </button>
+      <p aria-live="polite" className="sr-only" role="status"></p>
+    </form>
   );
 }
